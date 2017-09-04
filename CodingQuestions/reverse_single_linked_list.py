e6fbf497672930ee08c32bdac868be295f43ee1b
Joseph Nguyen <jnguy019@fiu.edu>
2017-09-04 11:55:28 -0400

    leet code question

---
+++ CodingQuestions/reverse_single_linked_list.py
@@ -0,0 +1,26 @@
+"""
+Reverse a singly linked list.
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        prev = None
+        next = None
+        while curr is not None:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
