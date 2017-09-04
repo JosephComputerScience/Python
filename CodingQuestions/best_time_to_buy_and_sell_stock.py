a1624c26ec0669e4a467e7cbda2261f9ce064314
Joseph Nguyen <jnguy019@fiu.edu>
2017-09-04 11:18:54 -0400

    increased ms for solution by 30%

--- CodingQuestions/best_time_to_buy_and_sell_stock.py
+++ CodingQuestions/best_time_to_buy_and_sell_stock.py
@@ -1,8 +1,13 @@
 """
-Say you have an array for which the ith element is the price of a given stock on day i.
-If you were only permitted to complete at most one transaction 
-(ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+Say you have an array for which the ith element is the
+price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction
+(ie, buy one and sell one share of the stock), design an algorithm
+to find the maximum profit.
 """
+
+
 class Solution(object):
     def maxProfit(self, prices):
         """
@@ -11,12 +16,10 @@ class Solution(object):
         """
         max_profit = 0
         buy_index = 0
-        sell_index = 0
-        for index in range(len(prices)):
-            profit = prices[index] - prices[buy_index]
+        for i, price in enumerate(prices):
+            profit = price - prices[buy_index]
             if profit > max_profit:
                 max_profit = profit
-                sell_index = index
-            elif prices[index] < prices[buy_index]:
-                buy_index = index
+            elif price < prices[buy_index]:
+                buy_index = i
         return max_profit
