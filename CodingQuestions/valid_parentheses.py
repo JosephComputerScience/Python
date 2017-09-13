14f25c718376e50aeaa33cd88e7a635dc9018b49
Joseph Nguyen <jnguy019@fiu.edu>
2017-09-13 17:25:47 -0400

    pep8 style fix

--- CodingQuestions/valid_parentheses.py
+++ CodingQuestions/valid_parentheses.py
@@ -5,6 +5,8 @@ determine if the input string is valid.
 The brackets must close in the correct order, "()" and "()[]{}"
 are all valid but "(]" and "([)]" are not.
 """
+
+
 class Solution(object):
     def isValid(self, s):
         """
