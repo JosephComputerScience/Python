178c2027bd241749447235915dc301b253e33dec
Joseph Nguyen <jnguy019@fiu.edu>
2017-09-13 17:25:03 -0400

    leetcode question

---
+++ CodingQuestions/valid_parentheses.py
@@ -0,0 +1,28 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}"
+are all valid but "(]" and "([)]" are not.
+"""
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stk = []
+        for ch in s:
+            if ch == '(' or ch == '{' or ch == '[':
+                stk.append(ch)
+            else:
+                if len(stk) == 0:
+                    return False
+                open_parenthesis = stk.pop()
+                if ch == ']' and open_parenthesis != '[':
+                    return False
+                elif ch == ')' and open_parenthesis != '(':
+                    return False
+                elif ch == '}' and open_parenthesis != '{':
+                    return False
+        return len(stk) == 0
